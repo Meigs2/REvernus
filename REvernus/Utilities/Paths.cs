7b01f5e57a6d05792f122711cc71475d015d6a0b
Meigs2 <ConnorRadeloff1@gmail.com>
2019-07-19 15:22:18 -0600

    Path.cs changes

--- REvernus/Utilities/Paths.cs
+++ REvernus/Utilities/Paths.cs
@@ -8,15 +8,16 @@ namespace REvernus.Utilities
 {
     public static class Paths
     {
+        // Directories
         public static string CurrentDirectory => Environment.CurrentDirectory;
         public static string DataFolderPath => Path.Combine(CurrentDirectory, "Data");
+        public static string DataBaseFolderPath => Path.Combine(DataFolderPath, "db");
 
         // DB Paths
-        public static string DataBaseFolderPath => Path.Combine(DataFolderPath, "db");
         public static string SdeDataBasePath => Path.Combine(DataBaseFolderPath, "eve.db");
         public static string CompressedSdeDataBasePath => Path.Combine(DataBaseFolderPath, "eve.db.bz2");
-
-
+        
+        // Specific Files
         public static string CharacterDataFilePath => Path.Combine(DataFolderPath, "Characters.bin");
         public static string SettingsDataFilePath => Path.Combine(DataFolderPath, "Settings.bin");
     }
