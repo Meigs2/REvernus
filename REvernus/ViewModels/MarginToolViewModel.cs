551b3c00a35abe29d082abdbf3fd5fbdbb804aca
Connor <Meigs2@users.noreply.github.com>
2019-08-29 19:28:50 -0600

    Move ExportedOrderModel to separate file

--- REvernus/ViewModels/MarginToolViewModel.cs
+++ REvernus/ViewModels/MarginToolViewModel.cs
@@ -2,6 +2,7 @@
 using Prism.Commands;
 using REvernus.Core;
 using REvernus.Utilities;
+using REvernus.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -461,22 +462,4 @@ namespace REvernus.ViewModels
             }
         }
     }
-}
-
-class ExportedOrderModel
-{
-    public double Price { get; set; }
-    public int VolumeRemaining { get; set; }
-    public int TypeId { get; set; }
-    public int Range { get; set; }
-    public long OrderId { get; set; }
-    public int VolumeEntered { get; set; }
-    public int MinVolume { get; set; }
-    public bool IsBuyOrder { get; set; }
-    public DateTime DateIssued { get; set; }
-    public int Duration { get; set; }
-    public long StationId { get; set; }
-    public int RegionId { get; set; }
-    public int SystemId { get; set; }
-    public int NumJumpsAway { get; set; }
 }
