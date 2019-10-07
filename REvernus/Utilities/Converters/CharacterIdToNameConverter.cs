5ad10b52e0f36a6c007da2d5138ae160746171d0
Connor <Meigs2@users.noreply.github.com>
2019-10-06 20:23:20 -0600

    Added more columns to the CitadelManagerView along with some converters.

---
+++ REvernus/Utilities/Converters/CharacterIdToNameConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using REvernus.Core;
+
+namespace REvernus.Utilities.Converters
+{
+    class CharacterIdToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var character = CharacterManager.CharacterList.FirstOrDefault(c =>
+                c.CharacterDetails.CharacterId == System.Convert.ToInt32(value));
+
+            return character?.CharacterName;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var character = CharacterManager.CharacterList.FirstOrDefault(c =>
+                c.CharacterName == System.Convert.ToString(value));
+
+            return character?.CharacterName;
+        }
+    }
+}
---
+++ REvernus/Utilities/Converters/TypeIdToNameConverter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace REvernus.Utilities.Converters
+{
+    public class TypeIdToNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            using var connection = new SQLiteConnection(DatabaseManager.ReadOnlyEveDbConnection);
+            connection.Open();
+
+            using var sqLiteCommand = new SQLiteCommand("SELECT typeName FROM invTypes WHERE typeId is @typeId", connection);
+            sqLiteCommand.Parameters.AddWithValue("@typeId", System.Convert.ToInt64(value));
+            try
+            {
+                using var reader = sqLiteCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    return System.Convert.ToString(reader[0]);
+                }
+                connection.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            using var connection = new SQLiteConnection(DatabaseManager.ReadOnlyEveDbConnection);
+            connection.Open();
+
+            using var sqLiteCommand = new SQLiteCommand("SELECT typeId FROM invTypes WHERE typeName is @typeName", connection);
+            sqLiteCommand.Parameters.AddWithValue("@typeName", System.Convert.ToString(value));
+            try
+            {
+                using var reader = sqLiteCommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    return System.Convert.ToInt64(reader[0]);
+                }
+                connection.Close();
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return null;
+        }
+    }
+}
