6c9bdcd39339d1c2e527d2b314fec9cd2fb0cecb
meigs2 <ConnorRadeloff1@gmail.com>
2019-10-14 14:38:30 -0600

    Added ability to remove citadels from the Citadel Manager

--- REvernus/ViewModels/CitadelManagerViewModel.cs
+++ REvernus/ViewModels/CitadelManagerViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
@@ -16,17 +17,22 @@ namespace REvernus.ViewModels
     {
         public CitadelManagerViewModel()
         {
-            
+            RemoveCitadelsCommand = new DelegateCommand<IList>(RemoveStructures);
+        }
+
+        private void RemoveStructures(IList structures)
+        {
+            CitadelManager.RemoveStructuresFromDatabase(structures);
         }
 
         public DelegateCommand AddCitadelsCommand { get; set; } = new DelegateCommand(AddNewCitadels);
+        public DelegateCommand<IList> RemoveCitadelsCommand { get; set; }
         private static void AddNewCitadels()
         {
             var citadelSearchWindow = new CitadelSearchWindow();
             citadelSearchWindow.ShowDialog();
 
             CitadelManager.InsertStructuresIntoDatabase(citadelSearchWindow.SelectedStructures);
-            CitadelManager.LoadCitadelsFromDatabase();
         }
     }
 }
