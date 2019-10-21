0558472faa1e0b7fcd7043a8d3cf52a0449c5bba
meigs2 <ConnorRadeloff1@gmail.com>
2019-10-21 15:15:47 -0600

    Removed unused using statement in StructureManager.cs

--- REvernus/Core/StructureManager.cs
+++ REvernus/Core/StructureManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data.SQLite;
-using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
