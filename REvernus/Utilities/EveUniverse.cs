2e791a023622b9dc6507fc03a5a8f0e9c20825aa
Sidoriuk Timur <SidoriukTY@tomskasu.ru>
2020-05-10 16:54:58 +0700

    fix utilites warnings

--- REvernus/Utilities/EveUniverse.cs
+++ REvernus/Utilities/EveUniverse.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data.SQLite;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using REvernus.Core;
-using REvernus.Core.ESI;
 
 namespace REvernus.Utilities
 {
